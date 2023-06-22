f43e8fd43af830f89ce5c87ca293b9241336e501
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2023-06-22 19:33:51 +0800

    Add files via upload

--- 体育/JS/88看球.js
+++ 体育/JS/88看球.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'88看球',
     // host:'http://www.88kanqiu.cc',
-    host:'http://www.888kanqiu.tv/',
+    host:'http://www.88kanqiu.one',
     url:'/match/fyclass/live',
     searchUrl:'',
     searchable:0,
