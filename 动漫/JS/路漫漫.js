7ffd52700e57b0d1e8b5347bdd4abe5a7898692d
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2023-10-01 12:31:28 +0800

    Add files via upload

--- 动漫/JS/路漫漫.js
+++ 动漫/JS/路漫漫.js
@@ -1,7 +1,7 @@
 var rule = {
 	title:'路漫漫',
 	// host:'https://www.96ba.com',
-	host:'https://www.yzxg68.com',
+	host:'https://www.lmm08.com',
 	// url:'/vod/show/id/fyclass/page/fypage.html',
 	url:'/vod/show/id/fyclassfyfilter.html',
 	filterable:1,//是否启用分类筛选,
