33086df4418d3fa8076890b80da00c2b162c83c8
Supprise0901 <141381309+Supprise0901@users.noreply.github.com>
2023-11-28 10:10:35 +0800

    Add files via upload

---
+++ hntv_m3u8/hntv_m3u8.py
@@ -0,0 +1,17 @@
+import json
+
+# Assuming video_data.txt contains a JSON-formatted string
+with open('video_data.txt', 'r', encoding='utf-8') as file:
+    data_json = file.read()
+    data = json.loads(data_json)
+
+
+# Extracting name and video_streams values
+with open('live.txt', 'w') as f:
+    for entry in data:
+        name = entry.get("name", "")
+        video_streams = entry.get("video_streams", "")
+        print(f'{name},{video_streams[0]}')
+        f.write(f'{name},{video_streams[0]}\n')
+
+    # print(f"Name: {name}\nVideo Streams: {video_streams}\n")
