060a21f9ad31d188aabcf1bd4ccb19eb02fef0f4
supprise <zzsupprise@proton.me>
2023-11-30 22:26:15 +0800

    modify dict

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -1,8 +1,12 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 import requests
 import re
 from bs4 import BeautifulSoup
 import os
+# 消除由urllib3库生成的警告，即在不验证SSL证书的情况下访问HTTPS网站
+import urllib3
+import mer_urls
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_url(name):
@@ -11,11 +15,16 @@ def get_url(name):
     }
     url = "http://tonkiang.us/"
     # 获取两页的m3u8链接
-    params = {
-        'page': 1,
-        "s": name
+    # params = {
+    #     "page": 1,
+    #     "s": name
+    # }
+    # response = requests.get(url, headers=headers, params=params, verify=False)
+    data = {
+        "search": name,
+        "Submit": " "
     }
-    response = requests.get(url, headers=headers, params=params, verify=False)
+    response = requests.post(url, headers=headers, data=data, verify=False)
     print(response)
     # print(response.text)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -29,17 +38,17 @@ def get_url(name):
 
         if url_match:
             extracted_url = url_match.group(1)
-            # print(extracted_url)
+            print(extracted_url)
             m3u8_list.append(extracted_url)
-        else:
-            print("URL extraction failed.")
+        # else:
+        #     print("URL extraction failed.")
     return m3u8_list
 
 
 def validate_m3u8_url(url):
     try:
         # 发送HTTP请求获取M3U8文件内容
-        response = requests.get(url, timeout=5)
+        response = requests.get(url, timeout=2)
         response.raise_for_status()
         if response.status_code == 200:
             valid_m3u8_link.append(url)
@@ -51,38 +60,39 @@ def validate_m3u8_url(url):
 
 
 # 检测有效链接，并写入m3u8_url.txt
-def detectLinks(name, m3u8_list):
-    # get_url(name)
+def detectLinks(name, m3u8_list, TV_name):
     # 多线程测试m3u8的链接有效性
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=3) as executor:
         futures = [executor.submit(validate_m3u8_url, m3u8_url) for m3u8_url in m3u8_list]
 
         # Wait for all tasks to complete
-        for future in futures:
-            future.result()
-        m3u8_list.clear()
-
-        # 检测的valid_m3u8_link列表，保存到m3u8_url.txt文本中
-        with open(os.path.join('TV', f'{name}.txt'), 'w', encoding='utf-8') as file:
-            for valid_url in valid_m3u8_link:
-                file.write(f'{name},{valid_url}\n')
-            valid_m3u8_link.clear()
+        # 等待所有任务完成
+        wait(futures)
+        # for future in futures:
+        #     future.result()
+    # 检测的valid_m3u8_link列表，保存到m3u8_url.txt文本中
+    with open(os.path.join(f'{TV_name}', f'{name}.txt'), 'w', encoding='utf-8') as file:
+        for valid_url in valid_m3u8_link:
+            file.write(f'{name},{valid_url}\n')
+        valid_m3u8_link.clear()
 
 
 if __name__ == '__main__':
+    tv_dict = {}
     valid_m3u8_link = []
-    tvDict = {}
-    # 读取文件并逐行处理
-    with open('tv_name.txt', 'r', encoding='utf-8') as file:
-        # with ThreadPoolExecutor(max_workers=5) as executor:
-        #     futures = [executor.submit(get_url, line.strip()) for line in file]
-        names = [line.strip() for line in file]
-        # Wait for all tasks to complete
-        for name in names:
-            print(name)
-            m3u8_list = get_url(name)
-            # 检测m3u8url是否有效，将有效url增加到列表
-            # print(m3u8_list)
-            tvDict[name] = m3u8_list
-            print(tvDict)
-            # detectLinks(name, m3u8_list)
+    # TV_names = ['🇨🇳央视频道', '卫视频道', '🇭🇰港台']
+    TV_names = ['卫视频道', '🇭🇰港台']
+    for TV_name in TV_names:
+        # 读取文件并逐行处理
+        with open(f'{TV_name}.txt', 'r', encoding='utf-8') as file:
+            names = [line.strip() for line in file]
+            for name in names:
+                m3u8_list = get_url(name)
+                tv_dict[name] = m3u8_list
+                print(name)
+            print(tv_dict)
+        for name, m3u8_list in tv_dict.items():
+            detectLinks(name, m3u8_list, TV_name)
+        # 合并m3u8链接
+        mer_urls.mer_links(TV_name)
+        tv_dict.clear()
