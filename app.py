326a05ff719aa09022d93cc1965b4b954a713141
supprise <zzsupprise@proton.me>
2024-02-04 21:23:10 +0800

    update modify

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open('live.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+    print(content)
+    return render_template('index.html', content=content)
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.3.8', debug=True)
