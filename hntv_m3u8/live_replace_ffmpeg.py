6ecbe50b56d58e4d74ced786a623268d7a8ef3f2
Supprise0901 <zzsupprise@proton.me>
2025-02-18 21:41:03 +0800

    modify

--- hntv_m3u8/live_replace_ffmpeg.py
+++ hntv_m3u8/live_replace_ffmpeg.py
@@ -15,13 +15,29 @@ def replace_content(live_file, live_ffmpeg_file, output_file):
     end_marker = "🇨🇳斗鱼电影,#genre#"
 
     start_index = live_content.find(start_marker)
-    end_index = live_content.find(end_marker)
+    start_line_end = -1
+    end_index = -1
+
+    if start_index != -1:
+        # 找到起始标记行的结束位置
+        start_line_end = live_content.find('\n', start_index)
+        if start_line_end == -1:
+            # 没有换行符，处理为文件末尾
+            start_line_end = len(live_content)
+        else:
+            # 包含换行符，移动到下一行开始
+            start_line_end += 1
+
+        # 在起始行之后查找结束标记
+        end_index = live_content.find(end_marker, start_line_end)
+    else:
+        print("未找到起始标记，未进行替换。")
 
     # 如果找到起始和结束标记
     if start_index != -1 and end_index != -1:
         # 替换内容
         new_content = (
-                live_content[:start_index]  # 保留起始标记之前的内容
+                live_content[:start_line_end]  # 保留起始标记行及之前的内容
                 + live_ffmpeg_content  # 插入 live_ffmpeg.txt 的内容
                 + live_content[end_index:]  # 保留结束标记之后的内容
         )
@@ -44,11 +60,8 @@ def main():
     parent_dir = os.path.dirname(current_dir)
     # 构造完整的文件路径
     live_file = os.path.join(parent_dir, "live.txt")
-    # print(live_file)
 
     # run_speed_ffmpeg生成的live_ffmpeg.txt
-    # live_ffmpeg_file = 'live_ffmpeg.txt'
-    # mian生成的live.txt
     live_ffmpeg_file = 'live.txt'
 
     # 执行替换
@@ -57,4 +70,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
